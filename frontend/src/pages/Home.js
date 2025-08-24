ffd5056b0ebb3a5764f403407d1d5a14e543d39a
“sha1am” <shadab171299@gmail.com>
2025-08-24 23:40:44 +0530

    removed bad code

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,11 +9,4 @@ const Home = () => (
     <Footer />
   </div>
 );
-
 export default Home;
-
-
-
-(base) shalam@Shadabs-MacBook-Pro android % echo $PATH
-/usr/local/sbin:/usr/local/sbin:/opt/anaconda3/bin:/opt/anaconda3/condabin:/usr/local/opt/curl/bin:/usr/local/opt/node@16/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/share/dotnet:~/.dotnet/tools:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
-(base) shalam@Shadabs-MacBook-Pro android % 
